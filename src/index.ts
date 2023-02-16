5e3462bb9adf298cbff792146eec89e88720e40a
layanemnzswok@gmail.com <layanemnzswork@gmail.com>
2023-02-15 21:46:11 -0300

    Endpoint - GET

--- src/index.ts
+++ src/index.ts
@@ -48,9 +48,10 @@ app.get("/users", async (req: Request, res: Response) => {
 
         } else {
             //const result = await db("users").where("email", "LIKE", `%${qUsers}%`) //busca por email, ex: "beltrana@emai.com"
-            const result = await db("users").where("name", "LIKE", `%${qUsers}%`) //busca por nome, ex: "fulano".
+            const result = await db("users").where("name", "LIKE", `%${qUsers}%`) //busca por nome, ex: "fulano"
             res.status(200).send(result) 
         }
+
         
 
     } catch (error) {
@@ -67,7 +68,6 @@ app.get("/users", async (req: Request, res: Response) => {
         }
     }
 })
-
 
 
 
